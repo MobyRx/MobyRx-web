a5c97a03833a86b50ffcaf44b1c7414243cf3cf7
ashqures <ashqures@cisco.com>
2016-09-30 01:34:26 +0530

    Master DB Util to execite master data

---
+++ src/main/java/com/MobyRx/java/util/MasterDBUtil.java
@@ -0,0 +1,11 @@
+package com.MobyRx.java.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ashqures
+ * Date: 9/30/16
+ * Time: 1:33 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MasterDBUtil {
+}
