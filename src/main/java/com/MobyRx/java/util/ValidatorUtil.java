34e84b45703013d40579b645466911b542cccdcc
ashqures <ashqures@cisco.com>
2016-09-30 01:13:32 +0530

    Validator util to handle all input data before proceeding

---
+++ src/main/java/com/MobyRx/java/util/ValidatorUtil.java
@@ -0,0 +1,71 @@
+package com.MobyRx.java.util;
+
+import com.MobyRx.java.service.wso.ClinicWSO;
+import com.MobyRx.java.service.wso.StatusWSO;
+import com.MobyRx.java.service.wso.UserWSO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ashif
+ * Date: 9/27/16
+ * Time: 12:12 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class ValidatorUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(ValidatorUtil.class);
+
+    private final static String PATTERN_PASSWORD = "^(?=.*\\d)(?=.*[A-Za-z])[A-Za-z0-9]{6,18}$";
+    private final static String PATTERN_EMAIL = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private final static String PATTERN_MOBILE = "^((\\+[1-9]?[0-9])|0)?[7-9][0-9]{9}$";
+    public static final String DATE_FORMAT = "dd/MM/yyyy";
+
+    public void validate(UserWSO userWSO, StatusWSO status) {
+
+    }
+
+    public void validate(ClinicWSO clinicWSO, StatusWSO status) {
+
+    }
+
+    public static boolean isValidEmail(String email) {
+        return email.matches(PATTERN_EMAIL);
+    }
+
+
+    public static boolean isValidPassword(String password) {
+        return password.matches(PATTERN_PASSWORD);
+    }
+
+    public static boolean isValidMobile(String mobile) {
+        return mobile.matches(PATTERN_MOBILE);
+    }
+
+
+    public static boolean isNullOrEmpty(String text) {
+        return null == text || "".equals(text);
+    }
+
+    public static boolean isValidDate(String dateToValidate) {
+        if (dateToValidate == null) {
+            return false;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+        sdf.setLenient(false);
+        try {
+            Date date = sdf.parse(dateToValidate);
+            logger.info("Date:- "+ date);
+        } catch (ParseException e) {
+            return false;
+        }
+        return true;
+    }
+}
